9587d647fcb71ee054e4c1863b03846f27ccb10e
Elie Schoppik <eschoppik@gmail.com>
2015-03-17 16:03:01 -0700

    cleaning up app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ bodyParser = require("body-parser");
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(methodOverride('_method'));
+app.use(methodOverride('_method'))
 
 
 // REGULAR GET
