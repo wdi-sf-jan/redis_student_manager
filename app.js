2985594010ce3337193343a8fb72e95f071f96ee
Elie Schoppik <eschoppik@gmail.com>
2015-03-19 11:01:53 -0700

    adding list solution

--- app.js
+++ app.js
@@ -8,95 +8,48 @@ bodyParser = require("body-parser");
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
 
 
 // REGULAR GET
 app.get('/', function(req,res){
-  // client.lrange("students",0, -1, function(err,students){
-    res.render('index');
-  // })
-});
-
-// AJAX GET
-app.get('/students.json', function(req,res){
   client.lrange("students",0, -1, function(err,students){
-    res.json(students);
-  })
-});
-
-// REGULAR POST
-
-// app.post('/add', function(req,res){
-//   client.lpush("students",req.body.name)
-//   res.redirect('/');
-// });
-
-// AJAX POST
-
-app.post('/add.json', function(req,res){
-  client.lpush("students",req.body.name)
-  res.json("ADDED")
-});
-
-// REGULAR DELETE_ONE
-
-// app.delete("/student/:name/", function(req,res){
-//   client.lrange("students",0,-1,function(err,students){
-//     students.forEach(function(student){
-//       if (req.params.name === student){
-//         client.lrem("students",1, student)
-//         res.redirect('/');
-//       }
-//     })
-//   });
-// })
-
-// AJAX UPDATE
-
-app.put("/student/:name.json", function(req,res){
-  client.lrange("students",0,-1,function(err,students){
-    console.log("GOT HERE!!")
-    students.forEach(function(student){
-      if (req.params.name === student){
-        console.log(req.params)
-        console.log(req.body)
-        client.lrem("students",1, student)
-        client.lpush("students",req.body.newName)
-        res.json('UPDATED');
-      }
-    })
+    res.render('index', {students:students});
   });
-})
-
-// AJAX DELETE_ONE
-
-app.delete("/student/:name.json", function(req,res){
-  client.lrange("students",0,-1,function(err,students){
-    students.forEach(function(student){
-      if (req.params.name === student){
-        console.log("HIT!")
-        client.lrem("students",1, student)
-        res.json('DELETED');
-      }
-    })
-  });
-})
-
-// AJAX DELETE ALL
-
-app.delete('/students.json', function(req,res){
-  client.del("students")
-  res.json('ALL DELETED');
 });
 
+app.get('/students/:name/edit', function(req,res){
+  client.lrange("students",0, -1, function(err,students){
+    // THIS IS WHY A LIST IS NOT THE BEST STRUCTURE!
+    students.forEach(function(student){
+      if(student === req.params.name){
+        res.render('edit', {student:student});
+      }
+    });
+  });
+});
 
-// REGULAR DELETE
+app.post('/create', function(req,res){
+  client.lpush("students",req.body.name);
+  res.redirect("/");
+});
 
-// app.delete('/students', function(req,res){
-//   client.del("students")
-//   res.redirect('/');
-// });
+app.put("/students/:name/", function(req,res){
+  // ADDITIONAL VALIDATION SHOULD GO HERE!
+  client.lrem("students",1, req.params.name);
+  client.lpush("students",req.body.newName);
+  res.redirect('/');
+});
+
+app.delete("/students/:name/", function(req,res){
+  client.lrem("students",1, req.params.name);
+  res.redirect('/');
+});
+
+app.delete('/students', function(req,res){
+  client.del("students");
+  res.redirect('/');
+});
 
 app.get('*', function(req,res){
   res.render('404');
